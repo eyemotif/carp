1a1de77f917b21fb5b5112616fcd3aa3956fa15d
aethine <yepicness@gmail.com>
2020-04-11 17:08:22 -0400

    Added change command and outputs

--- src/commands.rs
+++ src/commands.rs
@@ -25,7 +25,14 @@ pub fn list() {
         Err(e) => println!("{}", e),
     }
 }
-pub fn add(name: &str, version: Option<&str>) -> Result<(), Error> {
+pub fn add(name: &str, version: Option<&str>) -> Result<String, Error> {
+    if !cratesio::crate_exists(name) {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("Crate '{}' does not exist", name),
+        ));
+    }
+
     let latest = match cratesio::crate_latest(name) {
         Ok(v) => v,
         Err(e) => return Err(Error::new(ErrorKind::Other, format!("{:?}", e))),
@@ -56,9 +63,11 @@ pub fn add(name: &str, version: Option<&str>) -> Result<(), Error> {
         ));
     }
     dependencies.insert(String::from(name), String::from(ver));
-    return utils::write_dependencies(path, &dependencies);
+
+    utils::write_dependencies(path, &dependencies);
+    return Ok(format!("+ {} ({})", name, ver));
 }
-pub fn rem(name: &str) -> Result<(), Error> {
+pub fn rem(name: &str) -> Result<String, Error> {
     let path = &utils::get_toml_path();
     let mut dependencies = utils::read_parse_dependencies(path)?;
     match dependencies.remove_entry(name) {
@@ -70,5 +79,41 @@ pub fn rem(name: &str) -> Result<(), Error> {
             ))
         }
     };
-    return utils::write_dependencies(path, &dependencies);
+
+    utils::write_dependencies(path, &dependencies)?;
+    return Ok(format!("- {}", name));
+}
+pub fn change(name: &str, version: &str) -> Result<String, Error> {
+    let path = &utils::get_toml_path();
+    let mut dependencies = utils::read_parse_dependencies(path)?;
+    if !dependencies.contains_key(name) {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("Dependency '{}' not added", name),
+        ));
+    }
+
+    if !cratesio::crate_exists(name) {
+        return Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("Crate '{}' does not exist", name),
+        ));
+    }
+    match cratesio::crate_has_version(name, version) {
+        Ok(b) => {
+            if !b {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    format!("Crate '{}' does not have the version '{}'", name, version),
+                ));
+            }
+        }
+        Err(e) => return Err(Error::new(ErrorKind::Other, format!("{:?}", e))),
+    }
+    let old_version = dependencies
+        .insert(String::from(name), String::from(version))
+        .unwrap();
+
+    utils::write_dependencies(path, &dependencies)?;
+    return Ok(format!("* {} ({}) -> ({})", name, old_version, version));
 }
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         "list" => commands::list(),
         "add" => {
             if args.len() <= 2 {
-                println!("'carp add' requires at least 1 parameter ");
+                println!("'carp add' require 1 or 2 parameters");
                 return;
             }
             match commands::add(
@@ -26,17 +26,26 @@ fn main() {
                     Some(&args[3])
                 },
             ) {
-                Ok(_) => (),
+                Ok(message) => println!("{}", message),
                 Err(e) => println!("ERROR: {}", e),
             }
         }
         "rem" => {
             if args.len() <= 2 {
-                println!("'carp rem' requires at least 1 parameter");
+                println!("'carp rem' requires 1 parameter");
                 return;
             }
             match commands::rem(&args[2]) {
-                Ok(_) => (),
+                Ok(message) => println!("{}", message),
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        "change" => {
+            if args.len() <= 3 {
+                println!("'carp change' requires 2 parameters")
+            }
+            match commands::change(&args[2], &args[3]) {
+                Ok(message) => println!("{}", message),
                 Err(e) => println!("ERROR: {}", e),
             }
         }
