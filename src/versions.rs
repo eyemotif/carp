a71660fdcb8d76085892d698edffdeeab57ec4b9
aethine <yepicness@gmail.com>
2020-06-06 13:42:36 -0400

    Fixed version comparison

--- src/versions.rs
+++ src/versions.rs
@@ -3,5 +3,5 @@ use semver::Version;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub fn compare_version(current: &str, given: &str) -> Result<bool> {
-    return Ok(Version::parse(current)? < Version::parse(given)?);
+    return Ok(Version::parse(current)? >= Version::parse(given)?);
 }
